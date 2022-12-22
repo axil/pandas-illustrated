6bbb57b8826812d250a295601c9118decd68d8a9
Lev Maximov <lev.maximov@gmail.com>
2022-12-22 21:50:05 +0700

    more tests

--- pandas_illustrated/__init__.py
+++ pandas_illustrated/__init__.py
@@ -2,9 +2,15 @@
 
 def find(s, x, pos=False):
     if len(s) < 1000:
-        idx = s.tolist().index(x)
+        try:
+            idx = s.tolist().index(x)
+        except ValueError:
+            raise ValueError(f'{x!r} is not in Series')
     else:
-        idx = np.where(s == x)[0][0]
+        indices = np.where(s == x)[0]
+        if not len(indices):
+            raise ValueError(f'{x!r} is not in Series')
+        idx = indices[0]
     if pos:
         return idx
     else:
--- test.py
+++ test.py
@@ -1,14 +1,61 @@
 ﻿import pytest
+import numpy as np
 import pandas as pd
-import pandas_illustrated as pdi
+from pandas_illustrated import find, findall
 
-def test1():
-    s = pd.Series([4,2,4,6], index=['cat', 'penguin', 'dog', 'butterfly'])
-    assert pdi.find(s, 2) == 'penguin'
-    assert pdi.find(s, 4) == 'cat'
-    with pytest.raises(ValueError):
-        pdi.find(s, 3)
+def test_small():
+    s = pd.Series([4, 2, 4, 6], index=['cat', 'penguin', 'dog', 'butterfly'])
 
+    assert find(s, 2) == 'penguin'
+    assert find(s, 2, pos=True) == 1
+
+    assert find(s, 4) == 'cat'
+    assert find(s, 4, pos=True) == 0
+
+    with pytest.raises(ValueError) as exinfo:
+        find(s, 3)
+    assert str(exinfo.value) == '3 is not in Series'
+
+    with pytest.raises(ValueError) as exinfo:
+        find(s, 3, pos=True)
+    assert str(exinfo.value) == '3 is not in Series'
+    
+def test_large():
+    n = 10**4
+    a = np.arange(n)
+    np.random.shuffle(a)
+    s = pd.Series(a, index=np.arange(1, len(a)+1))
+    x = a[-1]
+    
+    assert find(s, x) == n
+    assert find(s, x, pos=True) == n-1
+    
+    with pytest.raises(ValueError) as exinfo:
+        find(s, n)
+    assert str(exinfo.value) == f'{n} is not in Series'
+
+    with pytest.raises(ValueError) as exinfo:
+        find(s, n, pos=True)
+    assert str(exinfo.value) == f'{n} is not in Series'
+
+def test_strings():
+    s = pd.Series(['dog', 'cat', 'penguin', 'cat'], index=list(range(1, 5)))
+    
+    assert find(s, 'dog') == 1
+    assert find(s, 'dog', pos=True) == 0
+
+    assert find(s, 'cat') == 2
+    assert find(s, 'cat', pos=True) == 1
+
+    x = 'butterfly'
+    with pytest.raises(ValueError) as exinfo:
+        find(s, x)
+    assert str(exinfo.value) == f'{x!r} is not in Series'
+
+    x = 'butterfly'
+    with pytest.raises(ValueError) as exinfo:
+        find(s, x, pos=True)
+    assert str(exinfo.value) == f'{x!r} is not in Series'
 
 if __name__ == '__main__':
     pytest.main(['-s', __file__])
