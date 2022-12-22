0082a7143fcd252010434748b41fcce915d2efef
Lev Maximov <lev.maximov@gmail.com>
2022-12-22 20:53:37 +0700

    init

---
+++ pandas_illustrated/__init__.py
@@ -0,0 +1,14 @@
+﻿import numpy as np
+
+def find(s, x, pos=False):
+    if len(s) < 1000:
+        idx = s.tolist().index(x)
+    else:
+        idx = np.where(s == x)[0][0]
+    if pos:
+        return idx
+    else:
+        return s.index[idx]
+
+def findall(s, x):
+    return s.index[np.where(s == x)[0]]
---
+++ test.py
@@ -0,0 +1,14 @@
+﻿import pytest
+import pandas as pd
+import pandas_illustrated as pdi
+
+def test1():
+    s = pd.Series([4,2,4,6], index=['cat', 'penguin', 'dog', 'butterfly'])
+    assert pdi.find(s, 2) == 'penguin'
+    assert pdi.find(s, 4) == 'cat'
+    with pytest.raises(ValueError):
+        pdi.find(s, 3)
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', __file__])
