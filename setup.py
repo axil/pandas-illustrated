1e5842e68dfebd13e3f006bc378437afce74909f
Lev Maximov <lev.maximov@gmail.com>
2022-12-23 00:17:21 +0700

    updated tests and setup

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pdi",
+    name="pandas-illustrated",
     version="0.1",
     author='Lev Maximov',
     author_email='lev.maximov@gmail.com',
     url='https://github.com/axil/pdi',
-    description="Helper functions from Pandas Illustrated article: find and findall",
+    description="Helper functions find() and findll() from Pandas Illustrated article",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['pandas'],
-    packages=['pdi'],
+    packages=['pandas_illustrated'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
