5896e71cf5322751cf2ffdd7a9c427a9900b7f04
Lev Maximov <lev.maximov@gmail.com>
2022-12-24 13:25:21 +0700

    insert()

--- pandas_illustrated/__init__.py
+++ pandas_illustrated/__init__.py
@@ -1,4 +1,5 @@
 ﻿import numpy as np
+import pandas as pd
 
 def find(s, x, pos=False):
     if len(s) < 1000:
@@ -18,3 +19,26 @@ def find(s, x, pos=False):
 
 def findall(s, x):
     return s.index[np.where(s == x)[0]]
+
+def insert(df, loc, row, index=None):
+    """
+    loc : int
+        Insertion index. Must verify 0 <= loc <= len(df).
+    row : list, tuple or dict
+    index : index value (if not provided index will be reset)"""
+    
+    n = len(df)
+    if not 0 <= loc <= n:
+        raise ValueError('Must verify 0 <= loc <= len(df)')
+    if isinstance(row, (list, tuple)):
+        df1 = pd.DataFrame([row], columns=df.columns, 
+                           index=[0 if index is None else index])
+    elif isinstance(row, dict):
+        df1 = pd.DataFrame(row, 
+                           index=[0 if index is None else index])
+    if loc==0:    # just for speed, not really necessary
+        return pd.concat([df1, df], ignore_index=index is None)
+    elif loc==n:  # also just for speed, not really necessary
+        return pd.concat([df, df1], ignore_index=index is None)
+    else:
+        return pd.concat([df[:loc], df1, df[loc:]], ignore_index=index is None)
---
+++ test_insert.py
@@ -0,0 +1,109 @@
+﻿import pytest
+import numpy as np
+import pandas as pd
+from pandas_illustrated import insert
+
+def test1():
+    df = pd.DataFrame([[4, 5, 6], [7, 8, 9]], columns=['A', 'B', 'C'])
+    row = [1, 2, 3]
+    
+    df1 = insert(df, 0, row)
+    assert df1.values.tolist() == [[1, 2, 3],
+                                   [4, 5, 6],
+                                   [7, 8, 9]]
+
+    df1 = insert(df, 1, row)
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [1, 2, 3],
+                                   [7, 8, 9]]
+
+    df1 = insert(df, 2, row)
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [7, 8, 9],
+                                   [1, 2, 3]]
+
+def test2():
+    df = pd.DataFrame([[4, 5, 6], [7, 8, 9]], columns=['A', 'B', 'C'])
+    row = {'A': 1, 'B': 2, 'C': 3}
+    
+    df1 = insert(df, 0, row)
+    assert df1.values.tolist() == [[1, 2, 3],
+                                   [4, 5, 6],
+                                   [7, 8, 9]]
+
+    df1 = insert(df, 1, row)
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [1, 2, 3],
+                                   [7, 8, 9]]
+
+    df1 = insert(df, 2, row)
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [7, 8, 9],
+                                   [1, 2, 3]]
+
+def test3():
+    df = pd.DataFrame([[4, 5, 6], [7, 8, 9]], columns=['A', 'B', 'C'])
+    with pytest.raises(ValueError) as exinfo:
+        insert(df, -1, [1, 2, 3])
+    assert str(exinfo.value) == 'Must verify 0 <= loc <= len(df)'
+
+def test4():
+    df = pd.DataFrame([[4, 5, 6], [7, 8, 9]], columns=['A', 'B', 'C'], index=['a', 'b'])
+    row = [1, 2, 3]
+    
+    df1 = insert(df, 0, row, 'c')
+    assert df1.values.tolist() == [[1, 2, 3],
+                                   [4, 5, 6],
+                                   [7, 8, 9]]
+    assert df1.index.tolist() == ['c', 'a', 'b']
+    
+    df1 = insert(df, 1, row, 'c')
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [1, 2, 3],
+                                   [7, 8, 9]]
+    assert df1.index.tolist() == ['a', 'c', 'b']
+    
+    df1 = insert(df, 2, row, 'c')
+    assert df1.values.tolist() == [[4, 5, 6],
+                                   [7, 8, 9],
+                                   [1, 2, 3]]
+    assert df1.index.tolist() == ['a', 'b', 'c']
+
+def test5():
+    df = pd.DataFrame([[4, 5., '6'], [7, 8., '9']], columns=['A', 'B', 'C'], index=['a', 'b'])
+    row = [1, 2., '3']
+    
+    df1 = insert(df, 0, row, 'c')
+    assert df1.values.tolist() == [[1, 2., '3'],
+                                   [4, 5., '6'],
+                                   [7, 8., '9']]
+    assert df1.index.tolist() == ['c', 'a', 'b']
+    
+    df1 = insert(df, 1, row, 'c')
+    assert df1.values.tolist() == [[4, 5., '6'],
+                                   [1, 2., '3'],
+                                   [7, 8., '9']]
+    assert df1.index.tolist() == ['a', 'c', 'b']
+    
+    df1 = insert(df, 2, row, 'c')
+    assert df1.values.tolist() == [[4, 5., '6'],
+                                   [7, 8., '9'],
+                                   [1, 2., '3']]
+    assert df1.index.tolist() == ['a', 'b', 'c']
+
+def test6():
+    df = pd.DataFrame([[4, 5, 6], [7, 8, 9]], 
+            columns=['A', 'B', 'C'], 
+            index=pd.MultiIndex.from_tuples([('a', 'x'), ('a', 'y')]))
+    row = [1, 2, 3]
+    df1 = insert(df, 0, row, ('b', 'z'))
+    assert df1.index.tolist() == [('b', 'z'), ('a', 'x'), ('a', 'y')]
+    
+    df1 = insert(df, 1, row, ('b', 'z'))
+    assert df1.index.tolist() == [('a', 'x'), ('b', 'z'), ('a', 'y')]
+    
+    df1 = insert(df, 2, row, ('b', 'z'))
+    assert df1.index.tolist() == [('a', 'x'), ('a', 'y'), ('b', 'z')]
+
+if __name__ == '__main__':
+    pytest.main(['-s', __file__])
