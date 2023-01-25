149f1a41a73810036ec8062a97642a25b366d93a
Lev Maximov <lev.maximov@gmail.com>
2023-01-26 04:04:49 +0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pandas-illustrated',
-    version='0.1',
+    version='0.2',
     author='Lev Maximov',
     author_email='lev.maximov@gmail.com',
     url='https://github.com/axil/pandas-illustrated',
-    description='Helper functions from the Pandas Illustrated Guide',
+    description='Helper functions from the Pandas Illustrated guide',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    python_requires=">=3.7",
     install_requires=[
         'pandas',
         'bs4',              # for patch_series
@@ -22,11 +23,16 @@ setuptools.setup(
     ],
     packages=['pdi'],
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',    
+        'Programming Language :: Python :: 3.10',    
+        'Programming Language :: Python :: 3.11',    
     ],
     license='MIT License',
     zip_safe=False,
-    keywords=['find', 'findall', 'insert', 'drop', 'sidebyside', 'pandas'],
+    keywords=['find', 'findall', 'insert', 'drop', 'levels', 'sidebyside', 'pandas'],
 )
