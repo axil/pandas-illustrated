a8a662a19cc677963dcc7bdd71bf1a4c13722887
Lev Maximov <lev.maximov@gmail.com>
2023-02-03 00:54:19 +0700

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pandas-illustrated',
-    version='0.3',
+    version='0.4',
     author='Lev Maximov',
     author_email='lev.maximov@gmail.com',
     url='https://github.com/axil/pandas-illustrated',
